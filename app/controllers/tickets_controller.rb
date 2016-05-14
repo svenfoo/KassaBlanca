92e5079ddd5a8fc83e7fb52989352aa7fc7e3783
Sven Neumann <sven@svenfoo.org>
2016-05-14 22:08:28 +0200

    Add basic views and check-in functionality

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,14 @@
 class TicketsController < ApplicationController
   def index
+    @tickets=Ticket.all
+  end
+  def show
+    @ticket=Ticket.find(params[:id])
+  end
+  def check_in
+    @ticket=Ticket.find(params[:ticket_id])
+    @ticket.checked_in_at=Time.now
+    @ticket.save
+    redirect_to @ticket
   end
 end

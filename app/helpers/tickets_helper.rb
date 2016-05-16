d9e07864f3e254913c5c3655c4f22a1857b301cc
Sven Neumann <sven@svenfoo.org>
2016-05-16 11:38:13 +0200

    Show number of checked-in tickets

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,16 @@
 module TicketsHelper
+
+  def number_of_tickets()
+    return @tickets.count
+  end
+
+  def number_of_tickets_checked_in()
+    return @tickets.where.not( checked_in_at: nil ).count
+  end
+
+  def number_of_tickets_checked_in_today()
+    today = Time.now.beginning_of_day..Time.now.end_of_day
+    return @tickets.where( checked_in_at: today ).count
+  end
+
 end

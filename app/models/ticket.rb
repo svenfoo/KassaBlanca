65e227c3a0da25c6c5a79d2da37714381c56002a
Daniel Mack <daniel@zonque.org>
2016-05-16 13:01:08 +0200

    move ticket helpers to model scopes

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,6 @@
 class Ticket < ActiveRecord::Base
+
+  scope :checked_in, -> { where.not(checked_in_at: nil) }
+  scope :checked_in_today, -> { where(checked_in_at: Time.now.beginning_of_day..Time.now.end_of_day) }
+
 end
